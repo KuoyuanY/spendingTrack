ecd6c34c20cdeb686bf11a58045d66071cf9e025
Ashwin Jeyaseelan <ashwinjey@gmail.com>
2017-02-18 06:42:26 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,8 +2,9 @@ function validate(){
    var username = document.getElementById("username").value;
    var password = document.getElementById("password").value;
    if (username == "username" && password == "password"){
+      alert("welcome!");
       window.location = "homepage.html";
-      return false;
+      return false
    }
    else {
       alert("Incorrect Username or Password!");
